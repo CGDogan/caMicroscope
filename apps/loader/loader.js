e7355a4373b297838783cd3a2301e3ecfea1f812
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-06-24 16:36:50 +0100

    dont show second error after retry

--- apps/loader/loader.js
+++ apps/loader/loader.js
@@ -187,7 +187,7 @@ function handleCheck(filename, reset, id, noRetry) {
       let destFilename = filename.replace('.', '_') + '_conv.tif';
       document.getElementById('filename'+0).value = destFilename;
       convertSlide(filename, destFilename).then((x)=>handleCheck(destFilename, reset, id, true))
-          .catch((err)=>changeStatus('CHECK', error, reset));
+          .catch((err)=>noRetry||changeStatus('CHECK', error, reset));
     } else {
       console.info('not retrying');
       changeStatus('CHECK', error, reset); // Handle the error response object
